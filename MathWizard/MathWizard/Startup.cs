cb9d5b63c998486a59d1b6b2546f5e2b03dbc14c
falma <falma@DESKTOP-KQ2MUB4>
2018-11-12 20:02:22 -0600

    new project

---
+++ MathWizard/MathWizard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MathWizard.Startup))]
+namespace MathWizard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
